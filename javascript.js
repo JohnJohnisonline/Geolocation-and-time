a90544d9ea049bbfc03cca4cbb6dcb997e4cba9e
JohnJohnisonline <jkamau1996@gmail.com>
2023-01-06 23:21:19 -0800

    Done

--- javascript.js
+++ javascript.js
@@ -39,11 +39,11 @@ function onLocationFound(e) {
   } else {
     L.circle(e.latlng, radius, { color: 'red' }).addTo(map);
   }
-    var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
-    var sunrise = times.sunrise.getHours();
-    var sunset = times.sunset.getHours();
+var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+var sunrise = times.sunrise.getHours();
+var sunset = times.sunset.getHours();
 
-    var currentTime = new Date().getHours();
+var currentTime = new Date().getHours();
         if (sunrise < currentTime && currentTime < sunset){
         map.removeLayer(dark);
         map.addLayer(light);
@@ -65,43 +65,43 @@ map.on('locationerror', onLocationError);
 map.locate({ setView: true, maxZoom: 16 });
 
 function showAlert() {
-    // Check if the "alertShown" cookie is set
-    if (!getCookie("alertShown")) {
-      // If the cookie is not set, show the alert and set the cookie
-      alert("This website does not collecting or store your information, we ask for location in order to optimize the map depending on the time of Day/Night.");
-      setCookie("alertShown", true, 365);
-    }
-  }
+        // Check if the "alertShown" cookie is set
+        if (!getCookie("alertShown")) {
+        // If the cookie is not set, show the alert and set the cookie
+        alert("This website does not collecting or store your information, we ask for location in order to optimize the map depending on the time of Day/Night.");
+        setCookie("alertShown", true, 365);
+        }
+}
   
   // Function to set a cookie
-  function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  }
+function setCookie(name, value, days) {
+var expires = "";
+        if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
   
   // Function to get a cookie
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
     }
-    return null;
-  }
   
   // Call the showAlert function when the page loads
-  window.onload = showAlert;
+window.onload = showAlert;
   
   // Layer Control
-  const baseLayers = {
-    'Nighttime Basemap': light,
-    'Daytime Basemap': dark
-};
+const baseLayers = {
+        'Nighttime Basemap': light,
+        'Daytime Basemap': dark
+        };
 const layerControl = L.control.layers(baseLayers).addTo(map);
